12d8765c4bac70a3cc5354f0e6eb103c76cb3dc6
JavierTorresH <javierthuitron22@gmail.com>
2024-05-27 16:37:20 -0600

    Add project files.

---
+++ SL_PL/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SL_PL.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SL_PL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SL_PL.WebApiApplication" Language="C#" %>
