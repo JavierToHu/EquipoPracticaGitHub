1c43a6239ca045ea4bd4b7a89f7ff5c5eaeb816f
GeorgeOaxaca1999 <noellefollyreal@gmail.com>
2024-05-28 14:35:57 -0600

    Se agregro una vista de Registrar Evento en el Pl, ademas de agregrar una api en SSL donde esta las peticiones del crud, como sus respectivos controladores.

---
+++ SL_PL/Controllers/EventoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SL_PL.Controllers
+{
+    public class EventoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Evento/GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            var result = BL.Evento.GetAllEvento();
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Evento/Add")]
+
+        public IHttpActionResult Add([FromBody] ML.Evento evento)
+        {
+            var result = BL.Evento.Add(evento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/Evento/Update")]
+
+        public IHttpActionResult Update([FromBody] ML.Evento evento)
+        {
+            var result = BL.Evento.Update(evento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("api/Evento/Delete")]
+
+        public IHttpActionResult Delete(int IdEvento)
+        {
+            var result = BL.Evento.Delete(IdEvento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/TipoEvento/GetById")]
+
+        public IHttpActionResult GetById(int IdEvento)
+        {
+            var result = BL.Evento.GetById(IdEvento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+    }
+}
---
+++ SL_PL/Controllers/TipoEventoController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SL_PL.Controllers
+{
+    public class TipoEventoController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/TipoEvento/GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            var result = BL.TipoEvento.GetAllTipoEvento();
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/TipoEvento/Add")]
+
+        public IHttpActionResult Add([FromBody] ML.TipoEvento tipoEvento)
+        {
+            var result = BL.TipoEvento.Add(tipoEvento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/TipoEvento/Update")]
+
+        public IHttpActionResult Update([FromBody] ML.TipoEvento tipoEvento)
+        {
+            var result = BL.TipoEvento.Update(tipoEvento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("api/TipoEvento/Delete")]
+
+        public IHttpActionResult Delete(int IdTipoEvento)
+        {
+            var result = BL.TipoEvento.Delete(IdTipoEvento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/TipoEvento/GetById")]
+
+        public IHttpActionResult GetById(int IdTipoEvento)
+        {
+            var result = BL.TipoEvento.GetById(IdTipoEvento);
+
+            if (result.Item1)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result.Item2);
+            }
+        }
+    }
+}
