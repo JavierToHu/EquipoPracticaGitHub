f062f2987a7a153ebfa3f8ee2c0dec2ca19a21a9
JavierTorresH <javierthuitron22@gmail.com>
2024-05-27 18:33:54 -0600

    Correcion ConnectionString ML a DL

--- DL/CRUDGitHub.Context.cs
+++ DL/CRUDGitHub.Context.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ML
+namespace DL
 {
     using System;
     using System.Data.Entity;
