d017839fc7dc77ed4d78671054bf01ffd77656c7
JavierTorresH <javierthuitron22@gmail.com>
2024-05-28 10:35:49 -0600

    Error de ML al DL por ConnectionString, ademas creacion de ML.TipoEvento y BL.TipoEvento

--- BL/TipoEvento.cs
+++ BL/TipoEvento.cs
@@ -8,6 +8,38 @@ namespace BL
 {
     public class TipoEvento
     {
-        
+        public (bool, string, List<ML.TipoEvento>, Exception) GetAllTipoEvento()
+        {
+            List<ML.TipoEvento> tipoEventos = new List<ML.TipoEvento>();
+
+            try
+            {
+                using (DL.EjercicioGitHubEntities context = new DL.EjercicioGitHubEntities())
+                {
+                    var query = context.GetAllTipoEvento().ToList();
+
+                    if (query != null)
+                    {
+                        foreach (var item in query)
+                        {
+                            ML.TipoEvento tipoEventoObj = new ML.TipoEvento();  
+                            tipoEventoObj.IdTipoEvento = item.IdTipoEvento;
+                            tipoEventoObj.Nombre = item.Nombre;
+
+                            tipoEventos.Add(tipoEventoObj);
+                        }
+                        return (true, null, tipoEventos, null);
+                    }
+                    else
+                    {
+                        return (false, null, tipoEventos, null);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message, null, ex);
+            }
+        }
     }
 }
